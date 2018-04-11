5643ee89f4d70dda29c6c93ef2aae97d656c33a5
Ismail Orabi <ismail.orabi@yale.edu>
2018-04-11 14:58:04 -0400

    integrate LTI framework

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <h1>Math Placement Calculator</h1>,
     document.getElementById('root')
 );
