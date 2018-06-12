c7629b3d2e1eb8696075575a0627cd4bfddbe566
Ismail Orabi <ismail.orabi@yale.edu>
2018-06-12 14:09:45 -0400

    reset rubric priority

--- grails-app/init/math/placement/tool/BootStrap.groovy
+++ grails-app/init/math/placement/tool/BootStrap.groovy
@@ -3,6 +3,15 @@ package math.placement.tool
 class BootStrap {
 
     def init = { servletContext ->
+        def allRubrics = Rubric.list()
+        def quizIds = allRubrics.quizId
+        quizIds.each{quizId ->
+            def quizRubrics = allRubrics.findAll{it.quizId == quizId}
+            quizRubrics.eachWithIndex{rubric,index ->
+                rubric.priority = index
+                rubric.save(flush:true)
+            }
+        }
     }
     def destroy = {
     }
