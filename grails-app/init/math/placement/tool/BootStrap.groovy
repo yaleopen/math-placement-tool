78eff36f0d376a84b46bee5ecaca596bf2b15dfa
Ismail Orabi <ismail.orabi@yale.edu>
2018-06-26 10:24:53 -0400

    fix create rubric bug, remove equation priority reset
    - add eachWithIndex

--- grails-app/init/math/placement/tool/BootStrap.groovy
+++ grails-app/init/math/placement/tool/BootStrap.groovy
@@ -3,17 +3,6 @@ package math.placement.tool
 class BootStrap {
 
     def init = { servletContext ->
-        def allRubrics = Rubric.list()
-        def quizIds = allRubrics.quizId
-        quizIds.each{quizId ->
-            def quizRubrics = allRubrics.findAll{it.quizId == quizId}
-            quizRubrics.each{rubric->
-                rubric.equations.eachWithIndex{ Equation equation, int i ->
-                    equation.priority = i
-                }
-                rubric.save(flush:true)
-            }
-        }
     }
     def destroy = {
     }
